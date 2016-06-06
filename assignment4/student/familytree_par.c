9abd576eca59f1ebf90eb7b6ba45a329bba380ab
nathanwbrei <nathan.w.brei@gmail.com>
2016-06-06 12:14:30 +0200

    Started assignment 4

--- assignment4/student/familytree_par.c
+++ assignment4/student/familytree_par.c
@@ -1,8 +1,18 @@
 #include "familytree.h"
+#include <omp.h>
 
-void traverse(tree *node, int numThreads){
-	
-	// put your code in here	
+void traverse(tree *node, int numThreads) {
+
+    if (node != NULL) {
+        node->IQ = compute_IQ(node->data);
+        genius[node->id] = node->IQ;
+
+        #pragma omp task firstprivate(node)
+        traverse(node->right, numThreads);
+
+        #pragma omp task firstprivate(node)
+        traverse(node->left, numThreads);
+    }
 
 }
 
