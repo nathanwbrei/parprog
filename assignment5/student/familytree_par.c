5dbb9bdc552e45c11bf6fcc508b2e8a78ec119eb
nathanwbrei <nathan.w.brei@gmail.com>
2016-06-06 13:02:49 +0200

    Started assignment 5

--- assignment5/student/familytree_par.c
+++ assignment5/student/familytree_par.c
@@ -2,7 +2,18 @@
 
 void traverse(tree *node, int numThreads){
 	
-	// put your code in here	
+    #pragma omp sections
+    {
+        if (node != NULL) {
+            node->IQ = compute_IQ(node->data);
+            genius[node->id] = node->IQ;
 
+            #pragma omp section
+            traverse(node->right, numThreads);
+
+            #pragma omp section
+            traverse(node->left, numThreads); 
+        }
+    }
 }
 
