92c845367b94b63041a15b6cfda7f1bd413c0873
nathanwbrei <nathan.w.brei@gmail.com>
2016-06-25 13:59:57 +0200

    Some code for assignments 7,8,9. Not sure if this is the correct/final version, but we'll figure that out later.

--- assignment7/student/loop_fission_par.c
+++ assignment7/student/loop_fission_par.c
@@ -1,15 +1,20 @@
 #include <omp.h>
+#include <stdio.h>
 
 void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N, int num_threads) {
 
-	// apply the loop transformation and use omp directives to get speedup
-
 	for (int j = 1; j < N; j++) {
+
+		#pragma omp parallel for num_threads(num_threads) schedule(static)
 		for (int i = 1; i < N; i++) {
-			a[i][j]   = a[i][j] * b[i][j+1]+d[i][j-1];
-			b[i][j] = 2 * a[i][j] * c[i - 1][j];
+			a[i][j] = a[i][j] * b[i][j+1] + d[i][j-1];
 			c[i][j] = 3 * d[i][j];
-			d[i][j]   = 2 * c[i-1][j];
+		}
+
+		#pragma omp parallel for num_threads(num_threads) schedule(static)
+		for (int i = 1; i < N; i++) {
+			b[i][j] = 2 * a[i][j] * c[i-1][j];
+			d[i][j] = 2 * c[i-1][j];
 		}
 	}
 }
--- assignment8/student/loop_alignment_par.c
+++ assignment8/student/loop_alignment_par.c
@@ -1,15 +1,24 @@
 #include <omp.h>
+#include <stdio.h>
 
 void compute(unsigned long *a, unsigned long *b, unsigned long *c, unsigned long *d, int N, int num_threads) {
 
-	// perform loop alignment to transform this loop and parallelize it with OpenMP	
-
-	for (int i = 1; i < N; i++) {
+	// printf("RUNNING PARALLEL VERSION\n\n");
+	
+	#pragma omp parallel for num_threads(num_threads) schedule(static)
+	for (int i = 1; i < N+1; i++) {
 		
-		a[i] = d[i] * b[i];
-		b[i + 1] = 2 * c[i];
-		c[i - 1] = a[i] * d[i];
-		
+		if (i > 1) {
+			b[i] = 2 * c[i-1];
+			// printf("b(%d) = 2 * c(%d)\n", i,i-1);
+		}
+		if (i < N) {
+			a[i] = d[i] * b[i];
+			c[i-1] = a[i] * d[i];
+			// printf("a(%d) = d(%d) * b(%d)\n", i,i,i);
+			// printf("c(%d) = a(%d) * d(%d)\n", i-1,i,i);
+		}
 	}
+	
 		
 }
